88b9dd43ce872d6e6df726e646fcec64061f03cb
= <=>
2023-01-30 18:58:51 +0100

    add new branch

---
+++ async_arace_task/index.js
@@ -0,0 +1,10 @@
+
+import { PageViewController } from "./src/components/PageViewController.js";
+
+
+let div = document.querySelector(".main");
+let pageViewController = new PageViewController(div);
+
+
+
+
